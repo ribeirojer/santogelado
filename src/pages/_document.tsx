75ff28380738253a202d10ccfc6b59a574eac5d1
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-06-16 14:44:51 -0300

    feat: edit head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,20 @@
-import Header from '@/components/Header'
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta
+          name="description"
+          content="Somos uma empresa de serviços de bartender"
+        />
+        <title>Santo Gelado Drinks</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
